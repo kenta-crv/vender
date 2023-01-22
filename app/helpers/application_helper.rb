7ead7ad8dda876823e0f874e0f6f16a624c986dc
kenta-crv <okuyamakenta@gmail.com>
2023-01-22 12:29:47 +0900

    tags

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,17 +4,13 @@ module ApplicationHelper
       site: "",
       title:"<%= yield(:title) | 自販機ねっと' %>",
       description: "自動販売機の一括比較見積もりなら『自販機ねっと』。条件の良い自販機会社を手軽に見積りできます。",
-      keywords: "自販機　設置,自動販売機　設置",
-      canonical: request.original_url,  # 優先されるurl
       charset: "UTF-8",
       reverse: true,
       separator: '|',
       icon: [
         { href: image_url('favicon.ico') },
         { href: image_url('favicon.ico'),  rel: 'apple-touch-icon' },
-      ],
-	    canonical: request.original_url  # 優先されるurl
-
+      ]
     }
   end
 
