c8aaf4f5f55434a62dbe0978fc688ffcfc39a7aa
kenta-crv <okuyamakenta@gmail.com>
2023-04-17 18:55:08 +0900

    fix

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -33,3 +33,10 @@ $(document).on("turbolinks:load", function () {
     window.location.href = path_name
   }
 })
+
+
+| (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+| new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+| j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+| 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+| })(window,document,'script','dataLayer','GTM-TVV7GHQ');
