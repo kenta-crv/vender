c77621c3d61f632b8af60e9226ecb02a38c7490c
kenta-crv <okuyamakenta@gmail.com>
2023-04-30 16:17:35 +0900

    column fix

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -24,38 +24,28 @@ class TopController < ApplicationController
   end
 
   def cocacola
-    render :layout => "cocacola"
-    @vender = "コカコーラ"
   end
 
   def asahi
-    render :layout => "asahi"
-    @vender = "アサヒ"
   end
 
   def otsuka
-    render :layout => "otsuka"
-    @vender = "大塚"
+
   end
 
   def itoen
-    render :layout => "itoen"
-    @vender = "伊藤園"
+
   end
 
   def dydo
-    render :layout => "dydo"
-    @vender = "ダイドー"
+
   end
 
   def kirin
-    render :layout => "kirin"
-    @vender = "キリン"
+
   end
 
   def suntory
-    render :layout => "suntory"
-    @vender = "サントリー"
   end
 
 end
