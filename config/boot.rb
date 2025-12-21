8c5f15c5c31bff6ecd36662a824ed95dbdb85271
kenta-crv <okuyamakenta@gmail.com>
2025-12-21 18:45:36 +0900

    contact fix

--- config/boot.rb
+++ config/boot.rb
@@ -1,4 +1,7 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
+# ここで「標準のLogger」をRailsより先に確定させます
+require 'logger'
+
 require 'bundler/setup' # Set up gems listed in the Gemfile.
 require 'bootsnap/setup' # Speed up boot time by caching expensive operations.
