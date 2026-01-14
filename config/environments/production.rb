2e1c67eb32ef7450974cde4c47dee5599e12f8f7
kenta-crv <okuyamakenta@gmail.com>
2026-01-14 21:13:21 +0900

    other

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,9 +4,8 @@ Rails.application.configure do
   # Code is not reloaded between requests.
   config.cache_classes = true
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = {host: "xn--new351c2sh.net"}
   config.hosts << "xn--new351c2sh.net"
-
+  config.relative_url_root = "/buy"
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
